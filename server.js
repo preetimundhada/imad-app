90671e60c5110d89046d69d24e90d5c974109cf5
preetimundhada <30768175+preetimundhada@users.noreply.github.com>
2017-08-08 16:35:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/artical_one', function (req, res) {
-  res.send('Artical one will be here in some time..... ');
+  res.sendFile(path.join(__dirname, 'ui', 'artical_one.html'));
 });
 app.get('/artical_two', function (req, res) {
   res.send('Artical two will be here in some time..... ');
